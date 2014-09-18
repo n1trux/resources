7e931400480d250fb63713fdf2fb89299572deb9
Arshad Chummun <arshad@chummun.com>
2014-09-18 11:38:31 +0100

    Updated all.json data

--- index.js
+++ index.js
@@ -6,14 +6,29 @@ var sourcesPath = './sources';
 fs.readdir(sourcesPath, function(error, files) {
   if (error) return console.log(error);
 
+  var all = {
+    "resources" : []
+  };
   files.forEach(function(f) {
     var info = require(sourcesPath + '/' + f);
 
     // Get the name the filename.
     info.name = f.replace(".json", "");
 
+    all.resources.push(info);
+
     // Create a Resource.
     var resource = new Resource(info);
-    resource.getJSONData();
+    resource.getJSONData(function(data) {
+      console.log(data);
+    });
+  });
+
+  // Write data to file.
+  fs.writeFile('./data/all.json', JSON.stringify(all, null, 4), 'utf8', function (error) {
+    if (error) return console.log(error);
+
+    // Log success message.
+    console.log("Successfully update all");
   });
 });
