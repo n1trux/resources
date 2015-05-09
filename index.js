ccbca2d1207cba87840a257d21a8bf93284192a2
Arshad Chummun <arshad@chummun.com>
2015-05-09 18:01:19 +0000

    Updated app to use gulp

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var Resource = require('./lib/resource');
 var sourcesPath = './sources';
+var handlebars = require('handlebars');
 
 // Read the sources directory and get data.
 fs.readdir(sourcesPath, function(error, files) {
@@ -20,14 +21,34 @@ fs.readdir(sourcesPath, function(error, files) {
     // Create a Resource.
     var resource = new Resource(info);
     resource.getJSONData(function(data) {
-      console.log(data);
+      var file = './data/' + data.name + '.json';
+
+      // Write data to file.
+      fs.writeFile(file, JSON.stringify(data, null, 4), 'utf8', function (error) {
+        if (error) return console.log(error);
+
+        // Log success message.
+        console.log("Successfully downloaded data for " + data.name);
+      });
+
+      // Update template.
+      fs.readFile('./templates/resources.html', 'utf8', function (err, source) {
+        if (err) { return console.log(err); }
+        var template = handlebars.compile(source, { noEscape: true });
+        var result = template(data);
+
+        fs.writeFile('./app/' + data.name + '.html', result, 'utf8', function (error) {
+          if (error) return console.log(error);
+          // Log success message.
+          console.log("Successfully generated " + data.name + ".html");
+        });
+      });
     });
   });
 
   // Write data to file.
   fs.writeFile('./data/all.json', JSON.stringify(all, null, 4), 'utf8', function (error) {
     if (error) return console.log(error);
-
     // Log success message.
     console.log("Successfully updated all.json");
   });
